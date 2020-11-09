6667bf9647c7a04c9146058db7806205e4f538f9
Alex Nagy <therebeloper@gmail.com>
2020-11-09 17:37:53 +0200

    Initial Commit

---
+++ CryptoManagerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoManagerDemo
+//
+//  Created by Alex Nagy on 09/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
